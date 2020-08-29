24b4c1f38d95cba61b428150aaf5c2582eae2be6
Kara Cavanaugh <karacav@msn.com>
2020-08-29 16:11:28 -0400

    Example 2- Reminder Service API

--- august.js
+++ august.js
@@ -8,3 +8,26 @@ const LoadAttributesRequestInterceptor = {
     }
   }
 } // also need an interceptor that saves the attributes
+
+// Example 2- Using Reminder Service API 
+  // check for previous reminder, delete if exists
+const previousReminder = sessionAttributes["reminderId"];
+if (previousReminder) {
+  try {
+    if (remindersList.totalCount !== "0") {
+      await reminderServiceClient.deleteReminder(previousReminder);
+      delete sessionAttributes["reminderId"];
+    }
+  }
+  catch (error) {
+    console.log("failed to delete " + {previousReminder} 
+      + " with error " + JSON.stringify(error));
+  }
+}
+  // create new reminder
+  const reminder = logic.createBirthdayReminder(
+    birthdayData.daysUntilBirthday,
+    timezone, 
+    Alexa.getLocale(requestEnvelope),
+    message )
+
